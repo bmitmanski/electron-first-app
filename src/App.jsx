af7127cf601a6b7eb916c22f5d1d28e843d2f381
Bartosz Mitmański <bartosz.mitmanski@acuris.com>
2018-03-16 13:09:51 +0100

    token

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as firebase from "firebase";
 import AddIntel from './AddIntel';
+import Auth from './Auth';
 import IntelList from './IntelList';
 
 class App extends React.Component {
@@ -8,19 +9,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     var config = {
-      apiKey: "",
+      apiKey: "AIzaSyDVSWdb1vg2F4s5X2eFzMthX6l2hQU_1AI",
       authDomain: "mitapp-1.firebaseapp.com",
       databaseURL: "https://mitapp-1.firebaseio.com",
       projectId: "mitapp-1",
       storageBucket: "mitapp-1.appspot.com",
       messagingSenderId: "1092311482785"
     };
+    console.log('App constructor initapp');
     firebase.initializeApp(config);
   }
 
   render() {
     return (<div>
       <h2>Welcome to React! mate 1</h2>
+      <Auth db={firebase}/>
       <AddIntel db={firebase}/>
       <IntelList db={firebase}/>
     </div>);
